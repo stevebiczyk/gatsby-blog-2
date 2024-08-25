baecbc7dea7d068e831a2d08afc7d45214a059d5
stevebiczyk <stevebiczyk@gmail.com>
2024-08-25 11:48:13 +0100

    created new page

---
+++ src/pages/page-3.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>HELLO FROM THE THIRD PAGE</h1>
+  </Layout>
+)
+
+export const Head = () => <Seo title="Page two" />
+
+export default ThirdPage
