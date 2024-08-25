533caad8aa4b9124c4e5d555770daa623104e448
stevebiczyk <stevebiczyk@gmail.com>
2024-08-25 13:49:34 +0100

    building the blog - part 3 + 4

---
+++ src/templates/blog-post.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <h4>{post.frontmatter.date}</h4>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
